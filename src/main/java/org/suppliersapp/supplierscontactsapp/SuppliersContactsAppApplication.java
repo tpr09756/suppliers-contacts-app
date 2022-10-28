bbd693a42587f38e2b13de227bfb4d738d1a79ba
Tiago Rodrigues <tpr09756@gmail.com>
2022-10-28 11:56:56 +0000

    maven and spring boot configuration

---
+++ src/main/java/org/suppliersapp/supplierscontactsapp/SuppliersContactsAppApplication.java
@@ -0,0 +1,13 @@
+package org.suppliersapp.supplierscontactsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliersContactsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliersContactsAppApplication.class, args);
+	}
+
+}
